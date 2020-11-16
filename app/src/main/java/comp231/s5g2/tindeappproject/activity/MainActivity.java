dd2c695a21e01ee73639bf066b5f2b50d349bb66
Felipe <beraldo.felipe@gmail.co>
2020-11-16 14:25:19 -0500

    [ADD] Dish list Layout
    Created the layout to show the Name, description and price for each dish in the menu. Still missing the pictures

--- app/src/main/java/comp231/s5g2/tindeappproject/activity/MainActivity.java
+++ app/src/main/java/comp231/s5g2/tindeappproject/activity/MainActivity.java
@@ -2,6 +2,7 @@ package comp231.s5g2.tindeappproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,10 +11,12 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -34,8 +37,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView restaurantPhone;
     private TextView restaurantName;
     private ImageView restaurantImage;
-    private ListView listDishes;
+    private RecyclerView listDishes;
     private double dishPrice;
+    private List<Dish> dishes = new ArrayList<Dish>();
+
 
     private String matchedRestaurantID;
 
@@ -47,11 +52,11 @@ public class MainActivity extends AppCompatActivity {
 
         matchedRestaurantID = "-MMDriFDJXNS9dSmrusC";
 
-        listDishes = findViewById(R.id.listView);
+
+
         restaurantName = findViewById(R.id.restaurantName);
         restaurantPhone = findViewById(R.id.restaurantPhone);
         restaurantImage = findViewById(R.id.imageView);
-
 
         DatabaseReference nameRef = myRef.child(matchedRestaurantID).child("restaurantName");
 
@@ -87,37 +92,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        listDishes = findViewById(R.id.DishesList);
+
         DatabaseReference dishesRef = myRef.child(matchedRestaurantID).child("dishes");
 
-//        dishesRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//
-//                restaurantName.setText(dataSnapshot.getValue(String.class));
-//                Log.d("restName", "Value is: " + restaurantName.getText());
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                // Failed to read value
-//                Log.w("restName", "Failed to read value.", error.toException());
-//            }
-//        });
 
+        dishesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
+                Dish dish = new Dish();
 
+                for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren())
+                    //Log.v("key node", "" + childDataSnapshot.getValue()); //displays the key for the node
+                    dish = childDataSnapshot.getValue(Dish.class);
+                    dishes.add(dish);
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-        //adaptor for the list
+            }
+        });
 
-   /*     ArrayAdapter<Dish> adapter = new ArrayAdapter<Dish>(
+       List<String> dishesNames = new ArrayList<String>();
+
+        for (Dish dish: dishes){
+
+            String dishName = dish.getNome();
+            dishesNames.add(dishName);
+        }
+
+      ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 getApplicationContext(),
-                android.R.layout.simple_list_item_2, android.R.id.text1,
-
-        );*/
+                android.R.layout.simple_list_item_2, android.R.id.text1, dishesNames);
 
 
-
+        listDishes.setAdapter(adapter);
 
 
 
