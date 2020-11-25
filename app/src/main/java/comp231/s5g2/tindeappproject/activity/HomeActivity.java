465aa462c8e2c2952017a98931f75175e0611338
kristine <codewithtine@gmail.com>
2020-11-25 17:00:39 -0500

    3rd commit from Kristine - Adding tempo home for ease of navigation

--- app/src/main/java/comp231/s5g2/tindeappproject/activity/HomeActivity.java
+++ app/src/main/java/comp231/s5g2/tindeappproject/activity/HomeActivity.java
@@ -1,13 +1,54 @@
 package comp231.s5g2.tindeappproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import comp231.s5g2.tindeappproject.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button btnAddDishes, btnFindingMatch, btnGroupRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //layout controls
+        init();
+
+        //button methods
+        btnAddDishes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),AddDishesActivity.class));
+            }
+        });
+
+        btnFindingMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),FindingMatchHomeActivity.class));
+            }
+        });
+
+        btnGroupRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainGroupRoomActivity.class));
+            }
+        });
+
+    }
+
+
+    private void init() {
+        btnAddDishes = findViewById(R.id.btnAddDishesActivity);
+        btnFindingMatch = findViewById(R.id.btnFindingMatchActivity);
+        btnGroupRoom = findViewById(R.id.btnCreateGroupRoom);
     }
 }
