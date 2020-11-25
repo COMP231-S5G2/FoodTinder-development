461ecac3035e49831097bd8e4e5ad03905b52a36
kristine <codewithtine@gmail.com>
2020-11-25 16:51:07 -0500

    2nd commit from Kristine - Add Finding Match and Group Room to Manifest

---
+++ app/src/main/java/comp231/s5g2/tindeappproject/activity/HomeActivity.java
@@ -0,0 +1,13 @@
+package comp231.s5g2.tindeappproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
